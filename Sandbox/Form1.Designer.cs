40e41fc69de32346058b37b942727f6deb46da2f
Allred.Sean@gmail.com <Allred.Sean@gmail.com@3597b27e-e6b4-f839-c08b-afe4dcced193>
2010-09-21 00:35:23 +0000

    Created a prototyping project in C# - also revamped the Function object in that C# prototype - this one should be alot more intuitive.

---
+++ Sandbox/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtIn
+            // 
+            this.txtIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIn.Location = new System.Drawing.Point(12, 12);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(514, 20);
+            this.txtIn.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(12, 38);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(514, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Begin Test";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.Location = new System.Drawing.Point(13, 68);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOut.Size = new System.Drawing.Size(513, 154);
+            this.txtOut.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 234);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtIn);
+            this.Name = "Form1";
+            this.Text = "Sandbox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ Sandbox/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Output(string s)
+        {
+            txtOut.Text += s;
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("You clicked my button!");
+            Output("Testing begun. " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + "\r\nInput: " + '"' + txtIn.Text + '"' + "\r\n");
+
+            ////
+            Function func = new Function(txtIn.Text, 'x');
+            foreach (Function.Arg_Part a in func.arg_list)
+                Output(a.value + " " + a.classification.ToString() + "\r\n\r\n");
+            ////
+
+            Output("Testing complete.\r\n\r\n");
+        }
+    }
+}
