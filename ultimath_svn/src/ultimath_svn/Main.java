415089e13855d127e1703074fdf6db340551dc54
Allred.Sean <Allred.Sean@3597b27e-e6b4-f839-c08b-afe4dcced193>
2010-10-10 23:17:53 +0000

    Java port 0.1.1 I forgot to actually add the Function class to the SVN hahah

---
+++ ultimath_svn/src/ultimath_svn/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ultimath_svn;
+
+/**
+ *
+ * @author seanallred
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I'm a Gnu");
+        Function fTest = new Function("2+5", 'x');
+    }
+
+}
