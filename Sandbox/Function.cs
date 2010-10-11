848c9031137536c2da47a38ca574c29daed4dcbf
Allred.Sean@gmail.com <Allred.Sean@gmail.com@3597b27e-e6b4-f839-c08b-afe4dcced193>
2010-10-11 16:26:03 +0000

    More progress on the prototype only

--- Sandbox/Function.cs
+++ Sandbox/Function.cs
@@ -7,13 +7,23 @@ namespace Sandbox
 {
     public class Function
     {
+        static long Factorial(long number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+
+        // Just had an idea to have predefined constants - want to write it down
+        // If we can use a flag character (I was thinking #) before a predefined keyword
+        // we can parse that out. Ex. #pir^2 -> 3.14159r^2
         #region Variable Members
 
         #region Private
 
         private string definition;
         private char parameter;
-
 
         Arg_Types Arg_Type_Of(char c)
         {
@@ -28,16 +38,37 @@ namespace Sandbox
                 return Arg_Types.Operator;
             if (_groupers.Contains(c))
                 return Arg_Types.Grouper;
+            if (c == '#')
+                return Arg_Types.Predefined;
             return Arg_Types.Unknown;
         }
         public struct Arg_Part
         {
             public string value;
             public Arg_Types classification;
+            private Operators opType;
             public Arg_Part(string Value, Arg_Types Classification)
             {
                 value = Value;
                 classification = Classification;
+                if (classification != Arg_Types.Operator)
+            }
+            public Operators getOperatorType() 
+            {
+                if (classification != Arg_Types.Operator)
+                    throw new ArgumentException("To use this function, the Arg_Part must be of type Operator");
+                switch (value)
+                {
+                    case "+": return Operators.ADD;
+                    case "-": return Operators.SUBTRACT;
+                    case "*": return Operators.MULTIPLY;
+                    case "/": return Operators.DIVIDE;
+                    case "%": return Operators.MODULO;
+                    case "^": return Operators.EXPONENT;
+                    case "!": return Operators.FACTORIAL;
+                    default:
+                        return Operators.NULL;
+                }
             }
         };
         // I believe the ICollection is the closest thing to a C# vector
@@ -46,6 +77,7 @@ namespace Sandbox
         private void Validate()
         {
             this.Definition = this.Definition.Replace(" ", ""); // Keep this in case I remember anything else we should strip
+            //this.Definition = this.Definition.Replace("#pi", Math.PI.ToString());
         }
         private void Tokenize()//Tokenize_StevensTest
         {
@@ -58,6 +90,21 @@ namespace Sandbox
             {
                 char c = definition[index];
                 Arg_Types type = Arg_Type_Of(c);
+
+                // This is the predefined constants parser... its completion level is like -45684%
+
+                //if (type == Arg_Types.Predefined)
+                //{
+                //    int start = index++;
+                //    for (; index < this.definition.Length; index++)
+                //    {
+                //        if (!Char.IsLetter(this.definition[index]))
+                //        {
+                //            arg_list.Add(new Arg_Part(this.definition.Substring(start, index - start), Arg_Types.Predefined));
+                //            break;
+                //        }
+                //    }
+                //}
 
                 // If its a variable then lets see if we had a variable last time we went through
                 if (type == Arg_Types.Constant)
@@ -125,27 +172,6 @@ namespace Sandbox
         #region Do Work, Son!
 
         /// <summary>
-        /// An archetype to perform a binary operation (5+4, 8*46, etc)
-        /// </summary>
-        /// <param name="one">The first operand</param>
-        /// <param name="op">The operator</param>
-        /// <param name="two">The second operand</param>
-        private void DoBinary(Arg_Part one, Operators op, Arg_Part two)
-        {
-            // must provide validation that it is an BINARY OPERATOR
-        }
-
-        /// <summary>
-        /// An archetype to perform a unary operation (5!, etc)
-        /// </summary>
-        /// <param name="arg">The operand</param>
-        /// <param name="op">The operater</param>
-        private void DoUnary(Arg_Part arg, Operators op)
-        {
-            // must provide validation that it is an UNARY OPERATOR
-        }
-
-        /// <summary>
         /// Status: Complete
         /// Insert implied operators in the arg_list so that 2x becomes 2*x
         /// </summary>
@@ -156,9 +182,88 @@ namespace Sandbox
             {
                 // if ConstantVariable OR VariableConstant
                 // may want to revise so that VariableConstant is parsed as Variable^Constant
-                if (((ops[index].classification == Arg_Types.Constant) || (ops[index].classification == Arg_Types.Variable)) &&
-                    ((ops[index+1].classification == Arg_Types.Variable) || (ops[index+1].classification == Arg_Types.Constant)))
+                if (((ops[index].classification == Arg_Types.Constant) && (ops[index+1].classification == Arg_Types.Variable)) ||
+                    ((ops[index].classification == Arg_Types.Variable) && (ops[index+1].classification == Arg_Types.Constant)))
                     ops.Insert(index + 1, new Arg_Part("*", Arg_Types.Operator));
+            }
+        }
+
+        /// <summary>
+        /// An archetype to perform a binary operation (5+4, 8*46, etc)
+        /// </summary>
+        /// <param name="one">The first operand</param>
+        /// <param name="op">The operator</param>
+        /// <param name="two">The second operand</param>
+        /// <remarks>
+        /// Binary Operator Parser Algorithm (+, -, *, /, ^, more?)
+        /// SWITCH CONTINUE(TRUE)
+        /// 
+        /// While CONTINUE
+        /// {
+        /// 	While iterating through the characters in a string
+        /// 		if this is the last character
+        /// 			Stop
+        /// 		if this character is an operator (this should never be the last character per the Function::Validate() function; to be written)
+        /// 			iterate left until a non-digit is found
+        /// 			store index of last digit found [LINDEX]
+        /// 			convert the substring from ([LINDEX] up to but not including the operator [INDEX-1]) to a double [LARGUMENT]
+        /// 			iterate right until a non-digit is found
+        /// 			store index of last digit found [RINDEX]
+        /// 			convert the substring from ([RINDEX] up to but not including the operator [INDEX+1]) to a double [RARGUMENT]
+        /// 			perform desired operation on [LARGUMENT] and [RARGUMENT] and convert the result to a string
+        /// 			remove the substring from [LINDEX] to [RINDEX] and replace it with the resulted string [at INDEX]
+        /// }
+        /// </remarks>
+        private Arg_Part DoBinary(Arg_Part one, Operators op, Arg_Part two)
+        {
+            if (op == Operators.FACTORIAL)
+                throw new ArgumentException("The FACTORIAL operator is unary; please provide a binary operator.");
+            switch (op)
+            {
+                case Operators.ADD:
+                    return new Arg_Part((Double.Parse(one.value) + Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                case Operators.SUBTRACT:
+                    return new Arg_Part((Double.Parse(one.value) - Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                case Operators.MULTIPLY:
+                    return new Arg_Part((Double.Parse(one.value) * Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                case Operators.DIVIDE:
+                    return new Arg_Part((Double.Parse(one.value) / Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                case Operators.MODULO:
+                    return new Arg_Part((Double.Parse(one.value) % Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                case Operators.EXPONENT:
+                    return new Arg_Part(Math.Pow(Double.Parse(one.value), Double.Parse(two.value)).ToString(), Arg_Types.Constant);
+                default:
+                    return new Arg_Part();
+            }
+        }
+
+        /// <summary>
+        /// An archetype to perform a unary operation (5!, etc)
+        /// </summary>
+        /// <param name="arg">The operand</param>
+        /// <param name="op">The operater</param>
+        private Arg_Part DoUnary(Arg_Part arg, Operators op)
+        {
+            if (op != Operators.FACTORIAL)
+                throw new ArgumentException("Please provide an unary operator.");
+            if (op == Operators.FACTORIAL && (int)Double.Parse(arg.value) != Double.Parse(arg.value))
+                throw new ArgumentException("Factorial requires an integer!");
+            return new Arg_Part((Double.Parse(arg.value)).ToString(), Arg_Types.Constant);
+        }
+
+
+        private void DoConstants(ref List<Arg_Part> ops)
+        {
+            for (int index = 1; index < ops.Count; index++)
+            {
+                if (ops[index].classification == Arg_Types.Operator)
+                {
+                    switch (ops[index].classification)
+                    {
+                        case Operators.ADD:
+
+                    }
+                }
             }
         }
 
@@ -207,10 +312,12 @@ namespace Sandbox
             return value;
         }
         #endregion
+
         #region Groupers
         public enum Arg_Types
         {
             Constant,
+            Predefined,
             Variable,
             Operator,
             Grouper,
@@ -225,7 +332,8 @@ namespace Sandbox
             DIVIDE = '/',
             EXPONENT = '^',
             MODULO = '%',
-            FACTORIAL = '!'
+            FACTORIAL = '!',
+            NULL
         }
         enum Groupers
         {
